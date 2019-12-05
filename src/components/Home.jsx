eed6ffcf67d761186576dced45903654e8716a6c
Deanabananapanda <dianaglezlag@gmail.com>
2019-12-05 17:00:54 -0600

    Diseño y estructura de la pagina home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom';
 
-function Home () {
-    return(
-        <img src="" alt=""/>   
-    )
+class Home extends Component{
+    render(){
+        return(
+            <div className="bosch">
+               <heder><img src="https://i.ibb.co/drgdHtH/Ribbet-Edit.png" className="Rainbow"></img></heder> 
+                    <img src="https://i.ibb.co/mDdR849/Whats-App-Image-2019-12-05-at-4-41-32-PM.jpg" className="boschLogo" alt=""></img>
+              <div className="">
+                <button className="botonInicio">START</button>
+
+                <div>
+                    <Link to="/Home">
+                    </Link>
+                    <Link to="/Home">
+                    </Link>
+                </div>
+                           
+            </div>
+            </div>
+
+        );
+
     }
+};
+
 export default Home;
