8e1a3bb69608b4bd23256b8279078a6a9c67cc7e
LauraMayaVayaHayaRaya <lauramaya86@gmail.com>
2019-12-05 17:36:43 -0600

    componente Qr

---
+++ src/components/Qr.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import QrReader from 'react-qr-reader'
+import QrDB from './QrData.js';
+import {
+	BrowserRouter as Router,
+  Switch,
+  Route,
+	Link,
+	Redirect
+} from "react-router-dom";
+
+class Qr extends Component {
+  state = {
+		result:false,
+		message:"ESCANEA TU CÓDIGO"
+  }
+
+  handleScan = data => {
+    if (data) {
+			const isMember = QrDB.find(member=>member.number===data)
+			if (isMember){
+				this.setState({
+					result: data
+				})
+			} else {
+				this.setState({
+					result:false, message:"USUARIO NO VÁLIDO"
+				})
+			}
+			console.log (isMember);
+
+			
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
+  render() {
+    return (
+      <div
+			>
+        <QrReader
+          delay={300}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '400px' }}
+          className="QrReader"
+        />
+				<p>
+					{this.state.message}
+				</p>
+				<div className="backHome">
+				<Link to="/">INICIO</Link>
+				</div>
+				{this.state.result ? <Redirect to="/success"/>:""}
+      </div>
+    )
+  }
+}
+
+export default Qr;
