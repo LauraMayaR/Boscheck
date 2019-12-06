f33b33a496c6837187779c7158b7518d6f8d822d
LauraMayaVayaHayaRaya <lauramaya86@gmail.com>
2019-12-06 14:08:50 -0600

    audio cambios

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import audio from '../inicio recorrido.ogg';
+import audio from '../inicio_recorrido.ogg';
 import { Link } from 'react-router-dom';
-
 function Sound1() {
     
     function play(){
        let  x = document.getElementById("myAudio"); 
        x.play(); 
     }
-
-
-
     return (
       <div className="bosch">
       <header>
         <img src="https://i.ibb.co/drgdHtH/Ribbet-Edit.png" className="Rainbow"></img>
-        <p>Hola</p>
       </header>
       <img src="https://i.ibb.co/mDdR849/Whats-App-Image-2019-12-05-at-4-41-32-PM.jpg" className="boschLogo" alt=""></img>
       <div className="">
@@ -23,15 +18,13 @@ function Sound1() {
         <source src={audio} type="audio/ogg"/>
         Your browser does not support the audio element.
       </audio>
-      <div>
-      <Link to="/BusStop">
-      <img src="https://cdn.dribbble.com/users/174627/screenshots/4891881/dribbble_jumpingpin.gif" className="ubicacion" alt=""></img>
-        <button onClick={play} className="botonInicio">INICIAR</button>
-        </Link>
+        <button onClick={play} className="botonInicio"><Link to="/"></Link>Play</button>
       </div >
-      </div>
       </div>
     );
   }
   
   export default Sound1;
+
+
+
