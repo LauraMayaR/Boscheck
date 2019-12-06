079b43e2524c69dd34a54ce87e5152d1e3a02a57
LauraMayaVayaHayaRaya <lauramaya86@gmail.com>
2019-12-06 15:51:58 -0600

    Updates

---
+++ precache-manifest.d14e80bb5114cd573cbbe65568213423.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64f4539d4182b07ecb29de0f57bf61e",
+    "url": "/Boscheck/index.html"
+  },
+  {
+    "revision": "b61e6f682933100b32b5",
+    "url": "/Boscheck/static/css/main.33cec924.chunk.css"
+  },
+  {
+    "revision": "c0a17be1cc5c5d3ea6d0",
+    "url": "/Boscheck/static/js/2.36ab8dbb.chunk.js"
+  },
+  {
+    "revision": "996b36b699aaa2670ad358b06f1cc5d8",
+    "url": "/Boscheck/static/js/2.36ab8dbb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b61e6f682933100b32b5",
+    "url": "/Boscheck/static/js/main.e14dc385.chunk.js"
+  },
+  {
+    "revision": "05aa69418a72006e01c3",
+    "url": "/Boscheck/static/js/runtime-main.c43846ff.js"
+  },
+  {
+    "revision": "ee83d0cb7f11bdc14745397ddcaebd91",
+    "url": "/Boscheck/static/media/bienvenido.ee83d0cb.ogg"
+  },
+  {
+    "revision": "436ad3bb71d27b1d2d19a4b704fce142",
+    "url": "/Boscheck/static/media/checked.436ad3bb.gif"
+  },
+  {
+    "revision": "1a70064cd600d00eeb3b1e9d888e2181",
+    "url": "/Boscheck/static/media/inicio recorrido.1a70064c.ogg"
+  }
+]);
