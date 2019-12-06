29d14a4a97cd5e997696cb341684300dedd2eec8
Deanabananapanda <dianaglezlag@gmail.com>
2019-12-06 12:27:01 -0600

    boton y ruta hacia el lector QR

--- src/components/BusStop.jsx
+++ src/components/BusStop.jsx
@@ -10,10 +10,13 @@ class BusStop extends Component{
   render(){
     return(
       <div className="bus">
-				<img src="https://images.squarespace-cdn.com/content/v1/500d7112e4b0a738c57eaa53/1543937703824-Z1LFH2PA8KQYKYJLKFTN/ke17ZwdGBToddI8pDm48kGVAqUhYrGdhEGudzFls7K1Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PI3qfRO8hJhbvfRCvNhjqrGe-PfXgVTpsvEoluIBTyvM0/bus.gif?format=1000w" alt=""/>
+
+      
+        <img src="https://images.squarespace-cdn.com/content/v1/500d7112e4b0a738c57eaa53/1543937703824-Z1LFH2PA8KQYKYJLKFTN/ke17ZwdGBToddI8pDm48kGVAqUhYrGdhEGudzFls7K1Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PI3qfRO8hJhbvfRCvNhjqrGe-PfXgVTpsvEoluIBTyvM0/bus.gif?format=1000w" alt=""/>
 				<Link to="/scanner">
           <button className="newStation">NUEVA ESTACION </button>
         </Link>
+    	
 
 				<div>
           <Link to="/Home">
