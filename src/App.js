05e9dacd214602195422e0bbaa0e9fe5e637d42c
LauraMayaVayaHayaRaya <lauramaya86@gmail.com>
2019-12-06 06:29:58 -0600

    correciones

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import {HashRouter, Route} from 'react-router-dom';
 import Home from './components/Home';
 import BusStop from './components/BusStop';
 import Scanner from './components/Scanner';
-<<<<<<< HEAD
+
 import Verified from './components/Verified';
-=======
-import Audio from './components/Audio';
->>>>>>> e3d83a73c3361e6d6654d658cc55dedbdf2b5bcb
+
+
+
 
 
 
@@ -21,12 +21,10 @@ export default function App() {
             <Route path="/" component={Home}/>
             <Route path="/BusStop" component={BusStop}/>
             <Route path="/Scanner" component={Scanner}/>
-<<<<<<< HEAD
+
             <Route path="/Verified" component={Verified}/>
-=======
-            <Route path="/Audio" component={Audio}/>
-          
->>>>>>> e3d83a73c3361e6d6654d658cc55dedbdf2b5bcb
+
+
           </React.Fragment>
         </HashRouter>
     </div>
